2e1677f2325673697a3550defe8d103156dcc76d
indraaristya <indraaristya98@gmail.com>
2019-03-19 22:38:41 +0800

    add jwt-auth #still error

---
+++ course-vid/sec-4/code/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+
+app = Flask(__name__)
+# app.secret_key = ""
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
+
+items = []
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item' : item}, 200 if item is not None else 404
+    def post(self, name):
+        if (next(filter(lambda x: x['name'] == name, items), None)) is not None:
+            return {'message' : "An item with name {} is already exist.".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price' : data['price']}
+        items.append(item)
+        return items, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
